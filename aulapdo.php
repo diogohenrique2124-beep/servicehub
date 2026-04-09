39328e1f32dcaedc9110aaa7dd7e19c759646d3d
Diogo Henrique Azevedo Souza <diogo.henrique.2124@gmail.com>
2026-04-09 15:38:02 -0300

    aula PDO pt1

---
+++ aulapdo.php
@@ -0,0 +1,22 @@
+<?php
+include_once "config/conexao.php";
+
+$sql = "select * from usuarios";
+$cmd = $pdo->prepare($sql);
+$cmd->execute();
+
+$servicos = $cmd->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($servicos)
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula PDO PHP</title>
+</head>
+<body>
+    <h2>Lista de Seriços</h2>
+</body>
+</html>
--- config/conexao.php
+++ config/conexao.php
@@ -1,14 +1,17 @@
 <?php
 
 $host = "10.91.47.39";
-$db = "servicehub01";
+$db = "servicehubdb01";
 $user = "root";
-$pass = "P@ssw0rd";
+$pass = "Aa123456";
 
 
 try{
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=uft8",$user,$pass);
-    //
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8",$user,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    // echo "conectado";
+    // var_dump($pdo);
 }catch(PDOException $e){
+    // var_dump($e->getMessage());
     die("Erro na conexão: ".$e->getMessage());
 }
